cb544bae9c02f9559d2966fd8dac067d5c72f353
thecoolmichael <michaeld1020@gmail.com>
2018-09-06 15:15:07 -0500

    update

--- mizzouImage.py
+++ mizzouImage.py
@@ -6,20 +6,23 @@ from IPython.display import display # for displaying our images
 # open our image (because everyone loves cats)
 rgb = Image.open('./mizzou.JPG') # the ./ refers to our local directory
 # what size is it?
-xsize, ysize = rgb.size # get the dimensionality
-green_img = np.array(rgb)
-green_img[:,:,0] = 0
-green_img[:,:,2] = 0
-image = Image.fromarray(green_img)
-# print is how we display statements to the user (text)
-#print( "Image size is " + str(pil_im.size) ) 
+hsv_im = rgb.convert("HSV")
+hsv = np.array(hsv_im)
 
-# show it 
-image.show()
-
-rgb_im = Image.fromarray( rgb )
-hsv_im = rgb_im.convert("HSV")
-hsv = np.array( hsv_im )
 hue = hsv[:,:,0]
 sat = hsv[:,:,1]
 val = hsv[:,:,2]
+xsize, ysize = hsv.size # get the dimensionality
+
+for i in range(xsize):
+  for j in range(ysize):
+      if( hue[i,j] > 95 or hue[i, j] < 80):
+          val[i, j] = 0
+          
+
+hsv_image = Image.merge("HSV", (Image.fromarray(hue), Image.fromarray(sat), Image.fromarray(val)))
+rbg_final = hsv_image.convert("RGB")
+rbg_final.show()
+
+# print is how we display statements to the user (text)
+#print( "Image size is " + str(pil_im.size) ) 
