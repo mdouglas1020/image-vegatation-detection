44c526fb03b4100cbff5bb51ff8757ec9c08aefe
Michael Douglas <michaeld1020@gmail.com>
2018-09-06 18:05:57 -0500

    RGB and HUE

--- mizzouImage.py
+++ mizzouImage.py
@@ -7,13 +7,18 @@ from IPython.display import display # for displaying our images
 rgb = Image.open('./mizzou.JPG') # the ./ refers to our local directory
 # what size is it?
 hsv_im = rgb.convert("HSV")
+rgb_arr = np.array(rgb)
+red = rgb_arr[:,:,0]
+green = rgb_arr[:,:,1]
+blue = rgb_arr[:,:,2]
+
 hsv = np.array(hsv_im)
 
 hue = hsv[:,:,0]
 sat = hsv[:,:,1]
 val = hsv[:,:,2]
 xsize, ysize = hue.shape # get the dimensionality
-lo,hi = 40, 200
+lo,hi = 40, 220
 lo = int((lo * 255) / 360)
 hi = int((hi * 255) / 360)
 
@@ -27,6 +32,15 @@ for i in range(xsize):
         
           
 hsv_image = Image.merge("HSV", (Image.fromarray(hue), Image.fromarray(sat), Image.fromarray(val)))
+hsv_array = np.array(hsv_image)
+hue2 = hsv_array[:,:,0]
+display(Image.fromarray( hue2 ).resize((math.floor(xsize/2),math.floor(ysize/2)),Image.BICUBIC))
+hue_image = Image.fromarray(hue2)
+#hue_image.show()
+#sat_image = Image.fromarray(sat)
+val_image = Image.fromarray(val)
+#sat_image.show()
+val_image.show()
 rbg_final = hsv_image.convert("RGB")
 rbg_final.show()
 
